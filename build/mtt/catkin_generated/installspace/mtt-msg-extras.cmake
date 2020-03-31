b2d5fb0b65ae84b84e78808bf651f523d8274ad0
Rui <ruipcosta@ua.pt>
2020-03-31 18:57:28 +0100

    cleanup

--- build/mtt/catkin_generated/installspace/mtt-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(mtt_MESSAGE_FILES "msg/Target.msg;msg/TargetList.msg;msg/TargetListPC.msg")
-set(mtt_SERVICE_FILES "")
--- build/velocityobstacle/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ruic7/MTT/src/velocityobstacle
-# Build directory: /home/ruic7/MTT/build/velocityobstacle
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
